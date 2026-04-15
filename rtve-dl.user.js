f86bea170f2ebd8ec1bf4c3f19c0c137c5a6cf12
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2026-04-15 21:30:26 +0200

    Create rtve-dl.user.js

---
+++ rtve-dl.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         RTVE Video and Subtitle Downloader 
+// @namespace    https://github.com/Myst1cX/rtve-dl
+// @version      1.0
+// @description  A RTVE downloader powered by downloadvideos.tv. Displays the downloadvideos.tv widget on RTVE videos. Works with both HLS and encrypted video streams. 
+// @author       Myst1cX
+// @match        https://www.rtve.es/*
+// @run-at       document-idle
+// @grant        none
+// @homepageURL  https://github.com/Myst1cX/rtve-dl
+// @supportURL   https://github.com/Myst1cX/rtve-dl/issues
+// @updateURL    https://raw.githubusercontent.com/Myst1cX/rtve-subs-dl/main/rtve-dl.user.js
+// @downloadURL  https://raw.githubusercontent.com/Myst1cX/rtve-subs-dl/main/rtve-dl.user.js
+// ==/UserScript==
